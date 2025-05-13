e22478f478b5dfe7fe544e22a129b0df92688fce
Phrojekt <phrojekt22@gmail.com>
2025-05-13 20:32:35 -0300

    ..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <head>
+        <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+      </head>
       <body className={`${abril.variable} ${averia.variable} ${openSans.variable}`}>
         {children}
       </body>
